bf1910f7b415f48a1e7af2d7ac73011e9fad7689
KamelZak <gk_zaknoune@esi.dz>
2021-10-03 00:37:07 +0100

    using the movie db API to display movies

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', 'App\Http\Controllers\MoviesController@index')->name('movies.index');
+Route::get('/movies/{id}', 'App\Http\Controllers\MoviesController@show')->name('movies.show');
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
